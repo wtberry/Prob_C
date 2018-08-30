b311317218de59a54bf36babee15c3dbb36a9997
wtberry <wtakahashi1@my.hpu.edu>
2018-08-29 15:22:06 -1000

    first version for hw1

---
+++ homework01/hw1.cpp
@@ -0,0 +1,8 @@
+// First C++ program, hw1, what should I create??
+# include <iostream>
+
+int main()
+{
+  std::cout << "Still thinking about what kind of program to write,\n";
+  std::cout << "Maybe Matrix calculator??";
+}
